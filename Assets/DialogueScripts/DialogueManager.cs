640bfb333e8721a222cc4347278e846eec91dff9
Shreya Pujari <trashTuxedoBirb@gmail.com>
2026-04-23 21:25:46 -0700

    Implemented Basic Dialogue Structure
    Made script that when prompted with E continues through a dialogue sequence

--- Assets/DialogueScripts/DialogueManager.cs
+++ Assets/DialogueScripts/DialogueManager.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
+using TMPro;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.InputSystem;
 
 [System.Serializable]
 public class Dialogue
@@ -12,4 +14,37 @@ public class Dialogue
 public class DialogueManager : MonoBehaviour
 {
     [SerializeField] private List<Dialogue> dialogues;
+    [SerializeField] private InputActionReference inputActionReference;
+    [SerializeField] private TextMeshProUGUI dialogueName;
+    [SerializeField] private TextMeshProUGUI dialogueText;
+    private int order;
+
+    void Start(){
+        order = -1;
+    }
+
+    void OnEnable(){
+        inputActionReference.action.Enable();
+        inputActionReference.action.performed += OnInteractPerformed;
+        inputActionReference.action.canceled += OnInteractCanceled;
+    }
+
+    void OnDisable(){
+        inputActionReference.action.performed -= OnInteractPerformed;
+        inputActionReference.action.canceled -= OnInteractCanceled;
+        inputActionReference.action.Disable();
+    }
+
+    private void OnInteractPerformed(InputAction.CallbackContext ctx){
+        
+    }
+
+    private void OnInteractCanceled(InputAction.CallbackContext ctx){
+        order += 1;
+        if (order < dialogues.Count){
+            dialogueName.text = dialogues[order].name;
+            dialogueText.text = dialogues[order].dialogueText;
+        }
+    }
+
 }
