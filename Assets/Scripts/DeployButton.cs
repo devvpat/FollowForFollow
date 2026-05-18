c90dae3b6b514cda3a994ea207bb1b65137fc0b5
SebastianCapuyanNSJ <capuyanj@uci.edu>
2026-05-17 19:30:46 -0700

    implmented delpoy button to go to scene floor1, and fixed boss trigger

--- Assets/Scripts/DeployButton.cs
+++ Assets/Scripts/DeployButton.cs
@@ -1,12 +1,20 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
+[RequireComponent(typeof(Button))]
 public class DeployButton : MonoBehaviour
 {
-    [SerializeField] private string targetSceneName = "SampleScene";
+    [SerializeField] private string targetSceneName = "Floor1";
+
+    void Awake()
+    {
+        GetComponent<Button>().onClick.AddListener(Deploy);
+    }
 
     public void Deploy()
     {
+        Debug.Log($"DeployButton.Deploy() fired. targetSceneName='{targetSceneName}'");
         if (string.IsNullOrEmpty(targetSceneName))
         {
             Debug.LogError("DeployButton: targetSceneName is empty.");
--- Assets/Scripts/Overworld/BossTrigger.cs
+++ Assets/Scripts/Overworld/BossTrigger.cs
@@ -3,14 +3,14 @@ using UnityEngine;
 public class BossTrigger : MonoBehaviour
 {
     [SerializeField] private GameObject battleUI;
+    [SerializeField] private EnemyParty enemyParty;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
-        {
-            PauseController.SetPause(true);
-            battleUI.SetActive(true);
-            BattleManager.Instance.StartNewFight();
-        }
+        if (!collision.CompareTag("Player")) return;
+
+        PauseController.SetPause(true);
+        battleUI.SetActive(true);
+        BattleManager.Instance.StartNewFight(enemyParty.Enemies);
     }
 }
