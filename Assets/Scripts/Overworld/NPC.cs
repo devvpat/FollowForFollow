ce1ff41fddff369defea133abf5098beaea82a15
Christopher <christopher2k05@outlook.com>
2026-05-19 16:20:56 -0700

    Implement dialogue/interaction system

--- Assets/Scripts/Overworld/NPC.cs
+++ Assets/Scripts/Overworld/NPC.cs
@@ -24,7 +24,7 @@ public class NPC : MonoBehaviour, IInteractable
 
         if (isDialogueActive)
         {
-            
+            NextLine();
         } else
         {
             StartDialogue();
